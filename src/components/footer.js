16c366d0c2be614bbb6ebbedab50a101ceb76af4
Masaya Shida <masayashida@gmail.com>
2021-06-09 16:28:16 +0700

    about page update, footer added social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import React from "react"
+import FbLogo from "../images/fb-logo.svg"
+import IgLogo from "../images/ig-logo.svg"
 import styles from "../styles/footer.module.css"
 
 export default function Footer() {
@@ -7,6 +9,14 @@ export default function Footer() {
             <p>
                 &#169; {new Date().getFullYear()} Ardent Phnom Penh
             </p>
+            <div>
+                <a href="https://www.facebook.com/ardent.phnompenh/">
+                    <img src={FbLogo} alt="FB logo" />
+                </a>
+                <a href="https://www.instagram.com/ardent.phnompenh/">
+                    <img src={IgLogo} alt="IG logo" />
+                </a>
+            </div>
         </div>
     )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,43 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import BackBtn from "../components/backBtn"
+import styles from "../styles/about.module.css"
 
 export default function About() {
+    const aboutData = useStaticQuery(graphql`
+        query AboutQuery {
+            allDatoCmsAboutInfo {
+                edges {
+                    node {
+                        id
+                        originalId
+                        heading
+                        body
+                    }
+                }
+            }
+    }`)
 
-
+console.log(aboutData)
     return (
         <Layout title="About">
             <div className="upperDiv">
                 <BackBtn destination="home"/>
                 <h1>About</h1>
             </div>            
+            <div className={styles.aboutContainer}>
+                {aboutData.allDatoCmsAboutInfo && aboutData.allDatoCmsAboutInfo.edges.map(edge => (
+                    <span>
+                        <h2>{edge.node["heading"]}</h2>
+                        <p>{edge.node["body"]}</p>
+                    </span>
+                ))}
+                <div classNAme={styles.contactContainer}>
+                    
+                </div>
+            </div>
+            
 
         </Layout>
     )
