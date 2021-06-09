9c211311151fa61cb30e7664e5b9fdd2bf7a6157
Masaya Shida <masayashida@gmail.com>
2021-06-09 19:52:06 +0700

    about page added contact info

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import BackBtn from "../components/backBtn"
 import styles from "../styles/about.module.css"
+import FbLogo from "../images/fb-logo.svg"
+import IgLogo from "../images/ig-logo.svg"
 
 export default function About() {
     const aboutData = useStaticQuery(graphql`
@@ -33,8 +35,20 @@ console.log(aboutData)
                         <p>{edge.node["body"]}</p>
                     </span>
                 ))}
-                <div classNAme={styles.contactContainer}>
-                    
+                <div className={styles.contactContainer + ` contactContainer`}>
+                    <h2 style={{fontWeight: 700}}>Contact</h2>
+                    <p style={{fontSize: `80%`}}>For business inquiries:</p>
+                    <a href="mailto:ardent.phnompenh@gmail.com">ardent.phnompenh@gmail</a>
+                    <p style={{fontSize: `80%`, marginTop: `.8rem`}}>Social media:</p>
+                    <span>
+                        <a href="https://www.facebook.com/ardent.phnompenh/">
+                            <img src={FbLogo} alt="FB logo" />
+                        </a>
+                        <a href="https://www.instagram.com/ardent.phnompenh/">
+                            <img src={IgLogo} alt="IG logo" />
+                        </a>
+                        <p>@ardent.phnompenh</p>
+                    </span>
                 </div>
             </div>
             
